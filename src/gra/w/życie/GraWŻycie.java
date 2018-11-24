e850b5f9f9ea8cfd38b50bfef059111d9fdfe5c6
Mariusz <s.mariusz@interia.pl>
2018-11-24 10:26:36 +0100

    Init repository

---
+++ src/gra/w/życie/GraWŻycie.java
@@ -0,0 +1,25 @@
+
+package gra.w.życie;
+
+public class GraWŻycie {
+
+    public static void main(String[] args) {
+        int x=5;
+        int y=5;
+        int tab[][]=new int[x][y];
+        for(int i=0;i<x;i++){
+            for(int j=0;j<y;j++){
+                tab[i][j]=0;
+            }
+        }
+        tab[1][1]=1;
+        for (int i=0;i<x;i++){
+            for (int j=0;j<y;j++){
+                System.out.print(tab[i][j]);
+            }
+            System.out.println();
+        }
+
+    }
+    
+}
